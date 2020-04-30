b9d1e55b15e1d509ba9e45ec4fb6805a397390c4
r144yh <mrakur@yandex.ru>
2020-04-30 21:20:05 +0300

    edit loginForm, add regForm, add favicon

--- forms.py
+++ forms.py
@@ -1,12 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField, DecimalField
+from wtforms.fields.html5 import DateField
+from wtforms.validators import DataRequired, Length, Email, NumberRange
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired(),
-                                                Length(min=2, max=15, message='Неверный логин')])
-    password = PasswordField('Пароль', validators=[DataRequired(),
+    username = StringField('Логин', validators=[DataRequired(message='Неверный логин'),
+                                                Length(min=2, max=15, message='Неверная длина логина')])
+    password = PasswordField('Пароль', validators=[DataRequired(message='Неверный пароль'),
                                                    Length(min=2, max=25, message='Неверная длина пароля')])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Вход')
+
+
+class RegistrationForm(FlaskForm):
+    login = StringField('Логин', validators=[DataRequired(message='Обязательное поле'),
+                                             Length(min=2, max=15, message='Неверная длина логина')])
+    email = StringField('Email', validators=[DataRequired(message='Обязательное поле'),
+                                             Email(message='Проверьте правильность ввода адреса')])
+    password = PasswordField('Пароль', validators=[DataRequired(message='Обязательное поле'),
+                                                   Length(min=2, max=25, message='Неверная длина пароля')])
+    age = DateField('Дата рождения', format='%Y-%m-%d')
+    level = RadioField('Уровень подготовки', choices=[('Новичок', 'Новичок'), ('Средний', 'Средний'),
+                                                      ('Профи', 'Профи')])
+    submit = SubmitField('Зарегистрироваться')
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
-from flask_material import Material
+from flask import Flask, render_template, flash, redirect, url_for
 from config import Config
-from forms import LoginForm
+from forms import LoginForm, RegistrationForm
 
-app = Flask('__main__', template_folder='frontend/templates')
+app = Flask('__main__',
+            template_folder='frontend/templates',
+            static_folder='frontend/static',
+            static_url_path='')
 app.config.from_object(Config)
-Material(app)
 
 
 @app.route('/')
@@ -13,11 +14,24 @@ def main():
     return render_template('base.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('main'))
     return render_template('login.html', title='Вход', form=form)
 
 
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Welcome, Chicken')
+        return redirect(url_for('index'))
+    return render_template('registration.html', title='Registration', form=form)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
